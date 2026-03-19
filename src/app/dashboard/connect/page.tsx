17a3ebe606f454acbd0e1596a3bc8fff55129ba2
Mkobilan <matthew.kobilan@gmail.com>
2026-03-19 20:58:21 +0600

    updated permissions fb

--- src/app/dashboard/connect/page.tsx
+++ src/app/dashboard/connect/page.tsx
@@ -62,7 +62,8 @@ export default function ConnectAccountsPage() {
             });
           }
         }, { 
-          scope: 'email,public_profile,pages_show_list,pages_read_engagement,pages_manage_posts,instagram_basic,instagram_content_publish'
+          scope: 'email,public_profile,pages_show_list,pages_read_engagement,pages_manage_posts,instagram_basic,instagram_content_publish',
+          config_id: '1642865807168430'
         });
       } else {
         window.location.href = `/api/auth/${id}`
